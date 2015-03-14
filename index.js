35edf4a6957f08b3e477b3a0b4087276daa85df6
DEADB17 <DEADB17@gmail.com>
2015-03-14 11:59:11 -0400

    adds transition information
    a fsm defines a directed graph where the states are the nodes and the
    transitions the edges. it is generally useful to expose this information
    to client code.
    this additional argument is passed after existing arguments to avoid
    breaking existing code. all existing tests are unaffected.
    a fresh object is created on each call to avoid a mutable shared
    reference

--- index.js
+++ index.js
@@ -11,23 +11,36 @@
 
     var callbacks = { before: {}, after: {}}
 
+    var transitionInfo = function (event, from, to, phase, isAny) {
+      return { event: event, from: from, to: to, phase: phase, isAny: isAny }
+    }
+
     var changeState = function(event){
-      var makeTransition = true;
+      var transition, makeTransition = true;
+      var from = publicMethods.state, to = endingState(event)
 
-      if (callbacks.before[event])
-        makeTransition = callbacks.before[event](publicMethods) !== false
+      if (callbacks.before[event]) {
+        transition = transitionInfo(event, from, to, 'before', false)
+        makeTransition = callbacks.before[event](publicMethods, transition) !== false
+      }
 
-      if (makeTransition && callbacks.before.any)
-        makeTransition = callbacks.before.any(publicMethods) !== false
+      if (makeTransition && callbacks.before.any) {
+        transition = transitionInfo(event, from, to, 'before', true)
+        makeTransition = callbacks.before.any(publicMethods, transition) !== false
+      }
 
       if (makeTransition) {
-        publicMethods.state = endingState(event)
+        publicMethods.state = to
 
-        if (callbacks.after[event])
-          callbacks.after[event](publicMethods)
+        if (callbacks.after[event]) {
+          transition = transitionInfo(event, from, to, 'after', false)
+          callbacks.after[event](publicMethods, transition)
+        }
 
-        if (callbacks.after.any)
-          callbacks.after.any(publicMethods)
+        if (callbacks.after.any) {
+          transition = transitionInfo(event, from, to, 'after', true)
+          callbacks.after.any(publicMethods, transition)
+        }
 
         return true
       }

2fdac1280ca4e4e815f4f6b88c87280e1808aa7b
DEADB17 <DEADB17@gmail.com>
2015-03-14 18:23:32 -0400

    merges changeState into publicMethods.trigger

--- index.js
+++ index.js
@@ -16,52 +16,50 @@
       return { event: event, from: from, to: to, phase: phase, isAny: isAny }
     }
 
-    function changeState(event, args){
-      var beforeEvent = before[event], beforeAny = before.any
-      var afterEvent, afterAny
-      var makeTransition = true;
-      var from = publicMethods.state, to = endingState(event)
-      // avoid making copies of the args array
-      // prepend publicMethods and a placeholder for transitionInfo
-      args.unshift(publicMethods, 1)
-
-      if (beforeEvent) {
-        args[1] = transitionInfo(event, from, to, 'before', false)
-        makeTransition = beforeEvent.apply(beforeEvent, args) !== false
-      }
-
-      if (makeTransition && beforeAny) {
-        args[1] = transitionInfo(event, from, to, 'before', true)
-        makeTransition = beforeAny.apply(beforeAny, args) !== false
-      }
-
-      if (makeTransition) {
-        publicMethods.state = to
-        afterEvent = after[event]
-        afterAny = after.any
-
-        if (afterEvent) {
-          args[1] = transitionInfo(event, from, to, 'after', false)
-          afterEvent.apply(afterEvent, args)
-        }
-
-        if (afterAny) {
-          args[1] = transitionInfo(event, from, to, 'after', true)
-          afterAny.apply(afterAny, args)
-        }
-
-        return true
-      }
-      return false
-    }
-
     publicMethods.canTrigger = function(event){
       return !!endingState(event)
     }
 
     publicMethods.trigger = function(event){
-      var args = Array.prototype.slice.call(arguments, 1)
-      return this.canTrigger(event) && changeState(event, args)
+      if (this.canTrigger(event)) {
+        var args = Array.prototype.slice.call(arguments, 1)
+        var beforeEvent = before[event], beforeAny = before.any
+        var afterEvent, afterAny
+        var makeTransition = true;
+        var from = publicMethods.state, to = endingState(event)
+        // avoid making copies of the args array
+        // prepend publicMethods and a placeholder for transitionInfo
+        args.unshift(publicMethods, 1)
+
+        if (beforeEvent) {
+          args[1] = transitionInfo(event, from, to, 'before', false)
+          makeTransition = beforeEvent.apply(beforeEvent, args) !== false
+        }
+
+        if (makeTransition && beforeAny) {
+          args[1] = transitionInfo(event, from, to, 'before', true)
+          makeTransition = beforeAny.apply(beforeAny, args) !== false
+        }
+
+        if (makeTransition) {
+          publicMethods.state = to
+          afterEvent = after[event]
+          afterAny = after.any
+
+          if (afterEvent) {
+            args[1] = transitionInfo(event, from, to, 'after', false)
+            afterEvent.apply(afterEvent, args)
+          }
+
+          if (afterAny) {
+            args[1] = transitionInfo(event, from, to, 'after', true)
+            afterAny.apply(afterAny, args)
+          }
+
+          return true
+        }
+      }
+      return false
     }
 
     publicMethods.before = function(event, callback) {

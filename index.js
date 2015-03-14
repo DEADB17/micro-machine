1842233dafdabea29034daa45a94ba6821273705
DEADB17 <DEADB17@gmail.com>
2015-03-14 19:05:46 -0400

    groups var definitions

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 (function(undefined) {
-  var BEFORE = 'before'
-  var AFTER = 'after'
-  var STATE = 'state'
-  var APPLY = 'apply'
-  var slice = [].slice
   function MicroMachine(initialState){
+    var STATE = 'state'
+    var BEFORE = 'before'
+    var AFTER = 'after'
+    var APPLY = 'apply'
+    var slice = [].slice
+
     var callbacks = {}
     var before = callbacks[BEFORE] = {}
     var after = callbacks[AFTER] = {}
@@ -22,14 +23,15 @@
     }
 
     publicMethods.trigger = function(event){
+      var args = slice.call(arguments, 1)
+      var beforeEvent = before[event]
+      var beforeAny = before.any
+      var afterEvent = after[event]
+      var afterAny = after.any
+      var makeTransition = true;
       var from = publicMethods[STATE]
       var to = transitionsFor[event][from]
       if (to) {
-        var args = slice.call(arguments, 1)
-        var beforeEvent = before[event]
-        var beforeAny = before.any
-        var afterEvent, afterAny
-        var makeTransition = true;
         // avoid making copies of the args array
         // prepend publicMethods and a placeholder for transitionInfo
         args.unshift(publicMethods, 1)
@@ -46,8 +48,6 @@
 
         if (makeTransition) {
           publicMethods[STATE] = to
-          afterEvent = after[event]
-          afterAny = after.any
 
           if (afterEvent) {
             args[1] = transitionInfo(event, from, to, AFTER, false)
